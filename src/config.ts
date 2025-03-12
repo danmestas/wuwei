031bb1b6e7e273af400dd4bd606a20fcd20d3fa6
Dan Mestas <93289084+danmestas@users.noreply.github.com>
2025-03-12 13:57:42 -0500

    Configure

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "", // replace this with your deployed domain
+  author: "Dan Mestas",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
